27afe6c0ae539221e72d1b1009a876ef7894d31f
bishalzogi <yogibishal19@gmail.com>
2024-11-09 10:20:21 -0500

    addded the veriables in main code

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	firstTask := "learn go programming language"
+	secondTask := "Learn docker"
+	thirdTask := "Learn kubernaties"
+	fourthTask := "learn terraform"
+	fifthTask := "learn CI/CD"
+
+	println(firstTask)
+	println(secondTask)
+	println(thirdTask)
+	println(fourthTask)
+	println(fifthTask)
+
+}
